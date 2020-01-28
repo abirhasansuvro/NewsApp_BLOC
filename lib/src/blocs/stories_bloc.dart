762ec2dbe3b989ab3b7b7555b5b5bb57546e03b2
unknown <abirorton@gmail.com>
2020-01-28 09:41:19 +0600

    New code 28th jan,2020

--- lib/src/blocs/stories_bloc.dart
+++ lib/src/blocs/stories_bloc.dart
@@ -1,4 +1,7 @@
 import 'package:rxdart/rxdart.dart';
+import 'package:rxdart/rxdart.dart' as prefix0;
+
+import 'dart:async';
 
 import '../models/item_model.dart';
 import '../resources/repository.dart';
@@ -6,15 +9,34 @@ import '../resources/repository.dart';
 class Stories{
   final _repository=Repository();
   final _topIds=PublishSubject <List<int>>(); 
+  final _items=BehaviorSubject<int>();
+
+  Observable<Map<int,Future<ItemModel>>> items;
 
   Observable<List<int>> get topIds=>_topIds.stream;
+
+  Stories(){
+    items=_items.stream.transform(_itemTransformer());
+  }
 
   fetchTopIds() async{
     final _ids=await _repository.fetchIds();
     _topIds.sink.add(_ids);
   }
 
+  Function(int) get fetchItm=>_items.sink.add;
+  _itemTransformer(){
+    return ScanStreamTransformer(
+      (Map<int,Future<ItemModel>>cache,int id,index){
+        cache[id]=_repository.fetchItem(id);
+        return cache;
+      },
+      <int,Future<ItemModel>>{},
+    );
+  }
+
   dispose(){
     _topIds.close();
+    _items.close();
   }
 }
--- lib/src/screens/news_list.dart
+++ lib/src/screens/news_list.dart
@@ -6,10 +6,29 @@ class NewsList extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
     final bloc=StoriesProvider.of(context);
+    bloc.fetchTopIds();
     return Scaffold(
       appBar: AppBar(
         title: Text('Top News'),
       ),
+      body: buildNews(bloc),
+    );
+  }
+
+  Widget buildNews(Stories bloc){
+    return StreamBuilder(
+      stream: bloc.topIds,
+      builder: (BuildContext context,AsyncSnapshot<List<int>> snapshot){
+        if(!snapshot.hasData)return Center(
+          child: CircularProgressIndicator(),
+        );
+        return ListView.builder(
+          itemCount: snapshot.data.length,
+          itemBuilder: (BuildContext context,int index){
+            return Text('${snapshot.data[index]}');
+          },
+        );
+      },
     );
   }
 }
